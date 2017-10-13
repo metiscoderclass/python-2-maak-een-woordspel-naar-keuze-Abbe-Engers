3d9d9f081c88ce02a53b3920d8087aa60f9eb20c
Abbe-Engers <abbeilias.engers@coderclass.nl>
2017-10-13 09:33:34 +0200

    got the _ game logics

--- galgje.py
+++ galgje.py
@@ -1,7 +1,13 @@
 import time
 import random
 
+aantalgeraden = 0
+
 stage = 0
+
+goedgeradenletters = []
+
+foutgeradenletters = []
 
 letterswoord = []
 
@@ -63,6 +69,9 @@ def stages(stage):
         print("  |")
         print("=========")
 
+for i in range(10):
+    print("")
+
 print("888")
 print("888")
 print("888")
@@ -75,7 +84,7 @@ print("                             888")
 print("                         Y8b d88")
 print("                          _Y88P")
 
-for i in range(9):
+for i in range(20):
     print(" ")
 
 print("TIP: Om het hele woord te raden type ?")
@@ -86,29 +95,57 @@ for i in range(20):
     print(" ")
 while True:
     stages(stage)
+    for i in range(20):
+        print("")
     if stage == 6:
         print("helaas, u bent af")
         break
-    for i in gebruikteletters:
+    for i in foutgeradenletters:
         print(i + ", ", end='')
     print(" ")
     letter = input("voer een letter in om het woord te raden : ")
     if letter == "qq":
         break
+    if letter == "?":
+        woordraden = input("voer een woord in om te raden : ")
+        if woordraden == geheimewoord:
+            print("je hebt het woord goed geraden")
+            break
+        else:
+            print("helaas")
+            stage+=1
     aantal = len(letter)
     if letter in "abcdefghijklmnopqrstuvwxyz" and aantal == 1:
         if letter not in geheimewoord:
             print("u heeft een foute letter gekozen, probeer het nog een keer!")
             stage += 1
+            foutgeradenletters.append(letter)
+
         elif letter in gebruikteletters:
             print("u heeft deze letter al gebruikt, gebruik een andere letter")
 
         elif letter in geheimewoord:
             print("letter zit in het woord!")
+            goedgeradenletters.append(letter)
         gebruikteletters.append(letter)
 
     else:
         print("potverdikkie dat is geen letter probeer een ECHTE letter!")
 
-    if letter in geheimewoord:
-        print(geheimewoord.replace(letter, '_'))
+    for i in range(30):
+        print("")
+
+    for a in geheimewoord:
+        if a in goedgeradenletters:
+            print(a, end=' ')
+            aantalgeraden+=1
+            print(aantalgeraden)
+        else:
+            print("_", end=' ')
+
+    if aantalgeraden == len(geheimewoord):
+        print("goedgeraden!!!")
+        break
+
+    for i in range(10):
+        print(" ")
